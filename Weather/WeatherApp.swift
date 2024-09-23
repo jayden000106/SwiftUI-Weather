cc64de6751a3c74621841b603dfae3c18536672b
정지혁 <cars0106@icloud.com>
2024-09-23 17:19:04 +0900

    initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 정지혁 on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
