a382a5b95d39f01cd0cacb0322dbf9489845cc51
정지혁 <cars0106@icloud.com>
2024-11-08 01:58:36 +0900

    feat: LocationListItem 구현
    - 애니메이션 안정화 필요

---
+++ Weather/Application/Features/LocationList/LocationListItem.swift
@@ -0,0 +1,53 @@
+//
+//  LocationListItem.swift
+//  Weather
+//
+//  Created by 정지혁 on 11/5/24.
+//
+
+import SwiftUI
+
+struct LocationListItem: View {
+    private let location: Location
+    
+    init(location: Location) {
+        self.location = location
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text(location.name)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .shadow(radius: 2)
+                    Text(location.tag ?? "")
+                        .foregroundStyle(Color.white.opacity(0.8))
+                        .font(.caption2)
+                }
+                Spacer()
+                Text("10°")
+                    .font(.largeTitle)
+                    .shadow(radius: 2)
+            }
+            HStack {
+                Text("대체로 청명함")
+                Spacer()
+                Text("최고: 13° 최저: 0°")
+            }
+            .font(.caption2)
+            .foregroundStyle(Color.white.opacity(0.8))
+        }
+        .padding(12)
+        .foregroundStyle(Color.white)
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color.blue)
+        }
+    }
+}
+
+#Preview {
+    LocationListItem(location: dummyLocations.first!)
+}
--- Weather/Application/Features/LocationList/LocationListView.swift
+++ Weather/Application/Features/LocationList/LocationListView.swift
@@ -8,39 +8,113 @@
 import SwiftUI
 
 struct LocationListView: View {
-    @Binding private var selectedItem: Item?
+    @Binding private var selectedLocation: Location?
+    
+    @State private var searchText = ""
     
     private let animation: Namespace.ID
     
-    init(selectedItem: Binding<Item?>, animation: Namespace.ID) {
-        self._selectedItem = selectedItem
+    init(selectedLocation: Binding<Location?>, animation: Namespace.ID) {
+        self._selectedLocation = selectedLocation
         self.animation = animation
     }
     
     var body: some View {
-        LazyVStack(spacing: 8) {
-            ForEach(items) { item in
-                VStack {
-                    HStack {
-                        Text(item.title)
-                        Spacer()
-                        Text(item.id.uuidString)
-                    }
-                    .padding()
-                }
-                .background(Color.red)
-                .matchedGeometryEffect(id: item.id, in: animation)
-                .onTapGesture {
-                    withAnimation {
-                        selectedItem = item
-                    }
+        ScrollView {
+            VStack(alignment: .leading, spacing: 12) {
+                toolbar
+                header
+                searchBar
+                locationList
+                caption
+            }
+            .padding(16)
+        }
+        .scrollIndicators(.hidden)
+        .scrollDisabled(true)
+    }
+}
+
+private extension LocationListView {
+    var toolbar: some View {
+        HStack {
+            Spacer()
+            Button {
+                
+            } label: {
+                Image(systemName: "ellipsis.circle")
+            }
+            .font(.title3)
+            .foregroundStyle(.primary)
+        }
+    }
+    
+    var header: some View {
+        Text("날씨")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+    }
+    
+    var searchBar: some View {
+        HStack(spacing: 4) {
+            Image(systemName: "magnifyingglass")
+            TextField("도시 또는 공항 검색", text: $searchText)
+            if !searchText.isEmpty {
+                Button {
+                    searchText = ""
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
                 }
             }
         }
+        .foregroundStyle(.secondary)
+        .padding(.vertical, 4)
+        .padding(.horizontal, 8)
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color(.secondarySystemBackground))
+        )
+    }
+    
+    var locationList: some View {
+        VStack {
+            ForEach(dummyLocations) { location in
+                LocationListItem(location: location)
+                    .matchedGeometryEffect(id: location.id, in: animation)
+                    .onTapGesture {
+                        withAnimation {
+                            selectedLocation = location
+                        }
+                    }
+            }
+        }
+    }
+    
+    var caption: some View {
+        HStack(spacing: 0) {
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("날씨 데이터")
+                    .underline()
+            }
+            Text(" 및 ")
+            Button {
+                
+            } label: {
+                Text("지도 데이터")
+                    .underline()
+            }
+            Text("에 관하여 더 알아보기")
+            Spacer()
+        }
+        .font(.caption2)
+        .foregroundStyle(Color.secondary)
     }
 }
 
 #Preview {
     @Previewable @Namespace var animation
-    LocationListView(selectedItem: .constant(nil), animation: animation)
+    LocationListView(selectedLocation: .constant(nil), animation: animation)
 }
